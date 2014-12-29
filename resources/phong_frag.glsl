0425a5a0c6734496d14116a441f05e6b86aa9c17
Jacinth Nguyen <jacinth@Jacinths-MacBook-Pro.local>
2014-12-29 12:50:32 -0600

    First Commit

--- resources/phong_frag.glsl
+++ resources/phong_frag.glsl
@@ -10,7 +10,7 @@ void main( void )
     
     vec3 ambient  = color * lightAmbt.xyz;
     vec3 diffuse  = color * lightDiff.xyz * max( dot( normal, L ), 0.0);
-    vec3 specular = color * lightSpec.xyz * pow( max( dot( R, V ), 0.0), 60.0f );
+    vec3 specular = color * lightSpec.xyz * pow( max( dot( R, V ), 0.0), 60.0 );
     
     gl_FragColor.xyz = ambient + diffuse + specular;
     gl_FragColor.a = 1.0;
